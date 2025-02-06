0bda6249d04a1d17a790b647185e3210e6cf5cd4
KM4HS <ac4hsu@gmail.com>
2025-02-06 22:14:13 +0900

    fix: 임포트 누락

--- src/components/NavPage.jsx
+++ src/components/NavPage.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {Link, useNavigate} from 'react-router-dom';
-import axiosInstance from "../api/api";
+import axiosInstance from './api/api';
 
 /**
  * 상단 탭 목록들
